1b157d9daa1cb0b7ee64c591635442314b1cacaf
PrashantRaj18198 <prashant@argonaut.dev>
2023-11-17 13:50:57 +0530

    feat: add daemon

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/warpbuilds/warpbuild-agent/pkg/app"
+)
 
 func main() {
 	if err := run(); err != nil {
@@ -9,5 +15,16 @@ func main() {
 }
 
 func run() error {
+	ctx := context.Background()
+
+	settingsFile := os.Getenv("WARPBUILD_AGENTD_SETTINGS_FILE")
+
+	err := app.NewApp(ctx, &app.ApplicationOptions{
+		SettingsFile: settingsFile,
+	})
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
