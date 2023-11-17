e6e7775954d32e59981addcbcd8eae7b1d729f77
PrashantRaj18198 <prashant@argonaut.dev>
2023-11-17 15:50:54 +0530

    feat: some scripts

---
+++ scripts/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+go build -o bin/warpbuild-agentd cmd/daemon/main.go
+cp bin/warpbuild-agentd /usr/local/bin/warpbuild-agentd
+cp tools/systemd/warpbuild-agentd.service /etc/systemd/system/warpbuild-agentd.service
+systemctl daemon-reload
+systemctl enable warpbuild-agentd
+systemctl start warpbuild-agentd
+
+echo "Using agent id: $AGENT_ID"
+cat <<EOF > /var/lib/warpbuild-agentd/settings.json
+{
+  "agent": {
+    "id": "$AGENT_ID"
+  },
+  "runner": {
+    "provider": "github",
+    "github": {
+      "runner_dir": "/runner",
+      "script": "run.sh",
+      "stdout_file": "/var/log/warpbuild-agentd/runner.github.stdout.log",  
+      "stderr_file": "/var/log/warpbuild-agentd/runner.github.stderr.log"
+    }
+  }
+}
+EOF
---
+++ tools/github/hooks/prerun.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export TEST_VAR="Hello from $WARPBUILD_RUNNER_SET_ID's pre processor!"
+echo "$TEST_VAR"
+
+echo $GITHUB_RUN_ID
+echo $GITHUB_RUN_ATTEMPT
+echo $GITHUB_JOB
+echo $GITHUB_REPOSITORY
+echo $WARPBUILD_RUNNER_NAME
+echo $RUNNER_OS
+
+if [ -z "$WARPBUILD_SCOPE_TOKEN" ]; then
+    echo "WARPBUILD_SCOPE_TOKEN is not set."
+    exit 1
+fi
+
+cat <<EOF > warpbuild_body.json
+{
+  "runner_id": "$WARPBUILD_RUNNER_SET_ID",
+  "runner_name": "$WARPBUILD_RUNNER_NAME",
+  "orchestrator_job_id": "$GITHUB_JOB_ID",
+  "orchestrator_job_group_id": "$GITHUB_RUN_ID",
+  "orchestrator_job_group_attempt": "$GITHUB_RUN_ATTEMPT",
+  "repo_entity": "$GITHUB_REPOSITORY"
+}
+EOF
+
+curl -X POST --silent --show-error --fail \
+     -H "Content-Type: application/json" \
+     -H "X-Warpbuild-Scope-Token: $WARPBUILD_SCOPE_TOKEN" \
+     -d @warpbuild_body.json \
+     $WARPBUILD_HOST_URL/api/v1/job
