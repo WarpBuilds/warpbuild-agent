0b6c13ba66d8fa1b0fece3c0217c8c7ccd9d1073
PrashantRaj18198 <prashant@argonaut.dev>
2023-11-20 14:55:45 +0530

    chore: add polling secret

--- pkg/manager/agent.go
+++ pkg/manager/agent.go
@@ -44,6 +44,7 @@ func (a *agentImpl) StartAgent(ctx context.Context, opts *StartAgentOptions) err
 		select {
 		case <-ticker.C:
 			log.Logger().Infof("checking for runner instance allocation details for %s", a.id)
+			log.Logger().Infof("polling secret: %s", a.pollingSecret)
 
 			allocationDetails, _, err := a.client.V1RunnerInstanceAPI.
 				GetRunnerInstanceAllocationDetails(ctx, a.id).
