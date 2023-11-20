3e987af096fb7ee24b6e3b2f96379f7a87194e10
Ankit <guptaankit015@gmail.com>
2023-11-20 17:56:25 +0530

    cleanup hook added

---
+++ pkg/hooks/cleanup.go
@@ -0,0 +1,57 @@
+package hooks
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/warpbuilds/warpbuild-agent/pkg/log"
+	"github.com/warpbuilds/warpbuild-agent/pkg/manager"
+	"github.com/warpbuilds/warpbuild-agent/pkg/warpbuild"
+)
+
+const CLEANUP_CALLBACK_HOOK string = "CLEANUP_CALLBACK_HOOK"
+
+type CleanupCallbackHook struct{}
+
+var _ manager.IPostEndHook = &CleanupCallbackHook{}
+
+func init() {
+	manager.RegisterHook[manager.IPostEndHook](&CleanupCallbackHook{})
+}
+
+func NewWarpBuildClient(opts *manager.AgentOptions) *warpbuild.APIClient {
+	cfg := warpbuild.NewConfiguration()
+	cfg.Servers[0].URL = opts.HostURL
+	return warpbuild.NewAPIClient(cfg)
+}
+
+// HookID implements manager.IPostEndHook.
+func (*CleanupCallbackHook) HookID() string {
+	return CLEANUP_CALLBACK_HOOK
+}
+
+// PostEndHook implements manager.IPostEndHook.
+func (*CleanupCallbackHook) PostEndHook(ctx context.Context, opts *manager.PostEndHookOptions) error {
+	ticker := time.NewTicker(5 * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			_, res, err := NewWarpBuildClient(opts.StartRunnerOptions.AgentOptions).V1RunnerInstanceAPI.
+				RunnerInstanceCleanupHook(ctx, opts.StartRunnerOptions.AgentOptions.ID).
+				XPOLLINGSECRET(opts.StartRunnerOptions.AgentOptions.PollingSecret).
+				Execute()
+			if err != nil {
+				log.Logger().Errorf("failed to get runner instance allocation details: %v", err)
+				return err
+			}
+			if res.StatusCode == http.StatusOK {
+				return nil
+			}
+		case <-ctx.Done():
+			log.Logger().Infof("Context cancelled. Agent is exiting...")
+			return nil
+		}
+	}
+	return nil
+}
