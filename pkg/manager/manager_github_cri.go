d4611b0d3c085f22eedcdd92b487fad70e2ba0f2
PrashantRaj18198 <prashant@argonaut.dev>
2024-11-01 15:48:23 +0530

    pass env via flag

--- pkg/manager/manager_github_cri.go
+++ pkg/manager/manager_github_cri.go
@@ -16,10 +16,11 @@ type ghcriManager struct {
 }
 
 type GithubCRIOptions struct {
-	StdoutFile string      `json:"stdout_file"`
-	StderrFile string      `json:"stderr_file"`
-	RunnerDir  string      `json:"runner_dir"`
-	CMDOptions *CMDOptions `json:"cmd_options"`
+	PassAllEnvs bool        `json:"pass_all_envs"`
+	StdoutFile  string      `json:"stdout_file"`
+	StderrFile  string      `json:"stderr_file"`
+	RunnerDir   string      `json:"runner_dir"`
+	CMDOptions  *CMDOptions `json:"cmd_options"`
 }
 
 type CMDOptions struct {
@@ -50,10 +51,12 @@ func (m *ghcriManager) StartRunner(ctx context.Context, opts *StartRunnerOptions
 		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", env.Key, env.Value))
 	}
 
-	log.Logger().Infof("Adding all available envs to command...")
-	for _, env := range os.Environ() {
-		log.Logger().Infof("env: %s", env)
-		cmd.Env = append(cmd.Env, env)
+	if m.PassAllEnvs {
+		log.Logger().Infof("Adding all available envs to command...")
+		for _, env := range os.Environ() {
+			log.Logger().Infof("env: %s", env)
+			cmd.Env = append(cmd.Env, env)
+		}
 	}
 
 	cmd.Dir = m.CMDOptions.Dir
